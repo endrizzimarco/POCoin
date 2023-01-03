34a01bbd43e01b99f50da596e9acd644bbaf6599
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-01-03 22:42:34 +0000

    feat: basic api implemented
    Co-authored-by: Georgi Ivanov <georgi-ivanov66@users.noreply.github.com>

--- backend/lib/web_server.ex
+++ backend/lib/web_server.ex
@@ -2,12 +2,15 @@ defmodule WebServer do
   import Plug.Conn
 
   def init(options) do
-    options
+    {nodes_pids, master_pid, wallet_pids} = Master.start()
   end
 
-  def call(conn, _opts) do
-    case conn.request_path do
-      "/hello" -> response(conn, "test")
+  def call(conn, pids) do
+    {nodes_pids, master_pid, wallet_pids} = pids
+
+    case conn.path_info do
+      ["balance", w] -> response(conn, Wallet.balance(get_pid(w)))
+      ["send", w, to, amount] -> response(conn, Wallet.send(get_pid(w), to, amount))
       "/world" -> response(conn, "lol")
       _ -> response(conn, "not found")
     end
@@ -17,5 +20,9 @@ defmodule WebServer do
     conn
       |> put_resp_content_type("application/json")
       |> send_resp(200, Jason.encode!(data))
+  end
+
+  defp get_pid(name) do
+    :global.whereis_name(String.to_atom(name))
   end
 end
