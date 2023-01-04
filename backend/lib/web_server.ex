50ce39d8a34cb1ee19753e1f356d68d3e6d2ad80
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-01-04 10:51:54 +0000

    feat: added send endpoint

--- backend/lib/web_server.ex
+++ backend/lib/web_server.ex
@@ -9,12 +9,16 @@ defmodule WebServer do
     {nodes_pids, master_pid, wallet_pids} = pids
 
     case conn.path_info do
-      # https://elixirstream.dev/regex/31910489-405c-40fb-aa4e-a9d4f285d57a - regex that matches ~balance?wallet=w1~
+
       ["balance"] ->
-        conn = Plug.Conn.fetch_query_params(conn)
-        params = conn.query_params
+        params = Plug.Conn.fetch_query_params(conn).query_params
         response(conn, Wallet.balance(get_pid(params["wallet"])))
-      ["send", w, to, amount] -> response(conn, Wallet.send(get_pid(w), to, amount))
+      ["send"] ->
+        params = Plug.Conn.fetch_query_params(conn).query_params
+        wallet = get_pid(params["wallet"])
+        to_addr = params["to"]
+        amount = elem(Float.parse(params["amount"]), 0)
+        response(conn, Wallet.send(wallet, to_addr, amount))
       ["world"] -> response(conn, "lol")
       _ -> response(conn, "not found")
     end
@@ -25,12 +29,6 @@ defmodule WebServer do
       |> put_resp_content_type("application/json")
       |> send_resp(200, Jason.encode!(data))
   end
-
-  # def response_with_text(conn, data) do
-  #   conn
-  #     |> put_resp_content_type("text/plain")
-  #     |> send_resp(200, data)
-  # end
 
   defp get_pid(name) do
     :global.whereis_name(String.to_atom(name))
