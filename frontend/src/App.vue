25a06c293ea67c04e2cde3b23fbe28f972d0f342
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-01-04 11:42:47 +0000

    feat: added tailwind and ant-design-vue
    Co-authored-by: Georgi Ivanov <georgi-ivanov66@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
 </script>
 
 <template lang="pug">
 header
   img(alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125")
+  a-button.bg-red-600(type="primary") Test
   .wrapper
     HelloWorld(msg="You did it!")
 main
