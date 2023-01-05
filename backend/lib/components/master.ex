26fd81629c59498c35e0d825ea57d9ea95c7b04a
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-01-05 01:14:41 +0000

    feat: added Charlie

--- backend/lib/components/master.ex
+++ backend/lib/components/master.ex
@@ -13,16 +13,20 @@ defmodule Master do
     master_pid = Wallet.start(String.to_atom("m"), Enum.at(nodes_pids, 0))
     Wallet.add_keypair(master_pid, {pub_key, priv_key})  # provide master with full genesis amount
 
-    # generate alice and bob wallets
-    wallet_pids = for x <- 1..2, do: Wallet.start(String.to_atom("w#{x}"), Enum.at(nodes_pids, x))
-    alice = Wallet.generate_address(Enum.at(wallet_pids, 0))
-    bob = Wallet.generate_address(Enum.at(wallet_pids, 1))
+    # generate alice, bob and charlie's wallets
+    wallet_pids = for x <- 1..3, do: Wallet.start(String.to_atom("w#{x}"), Enum.at(nodes_pids, x))
+    [alice, bob, charlie] = for w <- wallet_pids, do: Wallet.generate_address(w)
 
     Process.sleep(100) # let the master wallet realise he has control of the genesis amount
 
     # send 20 coins each to alice and bob
     Wallet.send(master_pid, alice, 20)
     Wallet.send(master_pid, bob, 20)
+    Wallet.send(master_pid, bob, 20)
+    Wallet.send(:global.whereis_name(:w2), alice, 1)
+    Wallet.send(:global.whereis_name(:w1), bob, 1)
+    Wallet.send(master_pid, bob, 1)
+
 
     {nodes_pids, master_pid, wallet_pids}
   end
